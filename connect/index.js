aeba2fabd3adb3ca1f9cf39fb17b4b7ba3238c06
zhenglinglu <l>
2020-06-26 12:07:31 +0800

    update all

--- connect/index.js
+++
@@ -1,8 +0,0 @@
-let mysql = require('mysql');
-let connection = mysql.createConnection({
-  host: '10.0.88.77',
-  user: 'root',
-  password: '123456',
-  database: 'collaboration_platform'
-});
-module.exports = connection;
