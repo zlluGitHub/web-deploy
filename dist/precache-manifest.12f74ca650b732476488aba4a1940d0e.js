b409403207f62ff423492b537294c22cd76d548b
zlluGitHub <18230086651@163.com>
2021-05-03 20:48:16 +0800

    add web

---
+++ dist/precache-manifest.12f74ca650b732476488aba4a1940d0e.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fda144cb97281bce790",
+    "url": "/css/app.951de3ad.css"
+  },
+  {
+    "revision": "dae1e8d1b5a2fd3933e9",
+    "url": "/css/chunk-161bfc83.a9339712.css"
+  },
+  {
+    "revision": "6ff05805f45f8b419475",
+    "url": "/css/chunk-21a06c17.c24983c8.css"
+  },
+  {
+    "revision": "2da011f4db7cb7637297",
+    "url": "/css/chunk-38426a76.7d77bf5e.css"
+  },
+  {
+    "revision": "1c27f49f38e03561327a",
+    "url": "/css/chunk-4a0577a7.dc987977.css"
+  },
+  {
+    "revision": "898e175509e030f79491",
+    "url": "/css/chunk-4a9884ac.366743f9.css"
+  },
+  {
+    "revision": "05b2ec055d00e84c1088",
+    "url": "/css/chunk-59728701.bb869b10.css"
+  },
+  {
+    "revision": "ced196142d53c52cb1e1",
+    "url": "/css/chunk-60918bd6.c4dbe94a.css"
+  },
+  {
+    "revision": "5431bebb76b651ef162e",
+    "url": "/css/chunk-766001c4.b0027977.css"
+  },
+  {
+    "revision": "9ae3df5f6ca8fbd885e4",
+    "url": "/css/chunk-b2db1ab2.f807b2f7.css"
+  },
+  {
+    "revision": "da8fa398df829d2215e8",
+    "url": "/css/chunk-e20850ce.b7dad62f.css"
+  },
+  {
+    "revision": "4efa2a6453b6345827bb",
+    "url": "/css/chunk-vendors.17c49282.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "3aebeef14039df8f4264dcdd1c018d20",
+    "url": "/img/bg1.3aebeef1.jpg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "85a00463924bdee840ea9cc989d1fc64",
+    "url": "/img/logo.85a00463.png"
+  },
+  {
+    "revision": "9cf6d6e9766d2026a9b7e418a2c0a589",
+    "url": "/img/logo15.9cf6d6e9.png"
+  },
+  {
+    "revision": "b1c166c3cb69e981e7e7353c9339cf08",
+    "url": "/img/sskoo.b1c166c3.png"
+  },
+  {
+    "revision": "ceec04c2ed0e2b3567be6704d04a6399",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fda144cb97281bce790",
+    "url": "/js/app.bd83e662.js"
+  },
+  {
+    "revision": "dae1e8d1b5a2fd3933e9",
+    "url": "/js/chunk-161bfc83.b2bc1a57.js"
+  },
+  {
+    "revision": "6ff05805f45f8b419475",
+    "url": "/js/chunk-21a06c17.819300b4.js"
+  },
+  {
+    "revision": "2da011f4db7cb7637297",
+    "url": "/js/chunk-38426a76.8a12b58d.js"
+  },
+  {
+    "revision": "1c27f49f38e03561327a",
+    "url": "/js/chunk-4a0577a7.adab8963.js"
+  },
+  {
+    "revision": "898e175509e030f79491",
+    "url": "/js/chunk-4a9884ac.c504429e.js"
+  },
+  {
+    "revision": "05b2ec055d00e84c1088",
+    "url": "/js/chunk-59728701.a807bf0c.js"
+  },
+  {
+    "revision": "ced196142d53c52cb1e1",
+    "url": "/js/chunk-60918bd6.2a4b6de9.js"
+  },
+  {
+    "revision": "5431bebb76b651ef162e",
+    "url": "/js/chunk-766001c4.81e41718.js"
+  },
+  {
+    "revision": "9ae3df5f6ca8fbd885e4",
+    "url": "/js/chunk-b2db1ab2.2665f590.js"
+  },
+  {
+    "revision": "da8fa398df829d2215e8",
+    "url": "/js/chunk-e20850ce.09920d85.js"
+  },
+  {
+    "revision": "4efa2a6453b6345827bb",
+    "url": "/js/chunk-vendors.34c2233e.js"
+  },
+  {
+    "revision": "cac439e5627fd04e389de3e7dd18fecb",
+    "url": "/manifest.json"
+  }
+]);
